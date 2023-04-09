777fe48e5c6c1b3f0c2bcfdb978596dc4cd13515
Kdnc00 <77608451+Kdnc00@users.noreply.github.com>
2023-04-09 16:12:38 +0300

    commit

--- src/main/java/kodlama/io/rentacar/adapters/FakePosServiceAdapter.java
+++ src/main/java/kodlama/io/rentacar/adapters/FakePosServiceAdapter.java
@@ -1,9 +1,10 @@
 package kodlama.io.rentacar.adapters;
 
 import kodlama.io.rentacar.business.abstracts.PosService;
+import org.springframework.stereotype.Service;
 
 import java.util.Random;
-
+@Service
 public class FakePosServiceAdapter implements PosService {
     @Override
     public void pay() {
